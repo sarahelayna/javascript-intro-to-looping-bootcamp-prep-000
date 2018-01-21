35bbfbdbea7e0161822589ecda8d122257cca6cc
Sarah F <sarahelayna@gmail.com>
2018-01-21 23:45:13 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -15,3 +15,12 @@ function whileLoop(number) {
   }
   return 'done';
 }
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+  return Math.random() >= 0.5
+} do {
+  
+} while (maybeTrue());
+return array;
+}
