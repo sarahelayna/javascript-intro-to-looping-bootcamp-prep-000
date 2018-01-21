7292983b495a268eeeb36eb08396c89fede39adf
Sarah F <sarahelayna@gmail.com>
2018-01-21 23:15:56 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,16 @@
+function forLoop(array) {
+var array = [];
+    if (i = 0) {
+    for (let i = 0; i < 25; i++)
+    console.log(`I am " + ${i} + " strange loops.`)
+    return array;
+  } else if (i = 1) {
+    for (let i = 0; i < 25; i++)
+    console.log("I am " + i + " strange loop.")
+    return array;
+  } else {
+    for (let i = 0; i < 25; i++)
+    console.log(`I am " + ${i} + " strange loops`)
+    return array;
+  }
+}
