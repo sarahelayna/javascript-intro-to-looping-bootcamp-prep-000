5c5d5c4a0ade99427c5fadaa0e7cab56cf21f9d4
Sarah F <sarahelayna@gmail.com>
2018-01-21 23:21:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,16 +1,9 @@
 function forLoop(array) {
-var array = [];
-    if (i = 0) {
-    for (let i = 0; i < 25; i++)
-    console.log(`I am " + ${i} + " strange loops.`)
-    return array;
-  } else if (i = 1) {
-    for (let i = 0; i < 25; i++)
-    console.log("I am " + i + " strange loop.")
-    return array;
-  } else {
-    for (let i = 0; i < 25; i++)
-    console.log(`I am " + ${i} + " strange loops`)
-    return array;
-  }
+    for (let i = 0; i < 25; i++) {
+      if (i = 1) {
+        array.push("I am " + i + " strange loop.")
+      } else {
+        array.push("I am " + i + " strange loops.")
+      }
+    }
 }
